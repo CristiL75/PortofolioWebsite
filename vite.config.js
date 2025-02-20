d33035cf1071b0471b7de712e3e769c86ba64f43
CristiL75 <68616866+CristiL75@users.noreply.github.com>
2025-02-21 00:25:35 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Numele exact al repository-ului tău
+  base: '/PortofolioWebsite', // Numele exact al repository-ului tău
 });
