b9ed4e2482b1cb3ca66fe7362d08e12674fe36c7
Latcu75 <cristilatcu7@yahoo.com>
2025-02-20 23:42:20 +0200

    pages correct 1

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/PortofolioWebsite/', // Numele exact al repository-ului tău
+  base: '/', // Numele exact al repository-ului tău
 });
